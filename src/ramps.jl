e3f360925784d5bb87a2418df757fc9517b4a544
haberdashpi <david.frank.little@gmail.com>
2019-08-27 09:29:37 -0400

    generalized ramp functions

--- src/ramps.jl
+++ src/ramps.jl
@@ -1,33 +1,37 @@
 
+export rampup, rampdown, ramp, fadeto
 
-function rampup_fn(len)
-    time = inseconds(len)
-    x -> x ≤ time : sinpi(0.5x/time) : 1.0
+function rampup_fn(x,len,fun)
+    time = inseconds(len,samplerate(x))
+    x -> x ≤ time ? fun(x/time) : 1.0
 end
 
-rampup(len) = x -> rampup(x,len)
-function rampup(x,len)
-    signal(rampup_fn(len),samplerate(x)) |> amplify(x)
+sinramp(x) = sinpi(0.5x)
+rampup(len::Number,fun::Function=sinramp) = x -> rampup(x,len,fun)
+function rampup(x,len::Number,fun::Function=sinramp)
+    signal(rampup_fn(len,fun),samplerate(x)) |> amplify(x)
 end
 
-function rampdown_fn(x,len)
-    time = inseconds(len)
-    x -> x < duration(x) - time : 1.0 : sinpi(0.5 + 0.5x/time)
+function rampdown_fn(x,len,fun)
+    time = inseconds(len,samplerate(x))
+    ramp_start = duration(x) - time
+    x -> x < ramp_start ? 1.0 : fun(1.0 - (x-ramp_start)/time)
 end
 
-rampdown(len) = x -> rampdown(x,len)
-function rampdown(x,len)
+rampdown(len::Number,fun::Function=sinramp) = x -> rampdown(x,len,fun)
+function rampdown(x,len::Number,fun::Function=sinramp)
     signal(rampdown_fn(len),samplerate(x)) |> amplify(x)
 end
 
-ramp(len) = x -> ramp(x,len)
-function ramp(x,len)
-    x |> rampup(len) |> rampdown(len)
+ramp(len,fun::Function=sinramp) = x -> ramp(x,len,fun)
+function ramp(x,len::Number,fun::Function)
+    x |> rampup(len,fun) |> rampdown(len,fun)
 end
 
-fadeto(y,len) = x -> fadeto(x,y,len)
-function fadeto(x,y,len)
-    silence = signal(zero,x) |> until(nsamples(x) - inframesof(len,x))
-    y = y |> rampup(len) |> prepend(silence)
-    x |> rampdown(len) |> mix(y)
+fadeto(y,len::Number,fun::Function=sinramp) = x -> fadeto(x,y,len,fun)
+function fadeto(x,y,len::Number,fun::aunction)
+    n = inframes(Int,len,samplerate(x))
+    silence = signal(zero,x) |> until(nsamples(x) - n)
+    y = y |> rampup(len,fun) |> prepend(silence)
+    x |> rampdown(len,fun) |> mix(y)
 end
