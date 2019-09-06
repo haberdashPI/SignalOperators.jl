2410de45eef3eff05e478da2d54f5243f734dfbc
haberdashpi <david.frank.little@gmail.com>
2019-09-06 11:55:28 -0400

    moved file stuff to WAV [ci skip]

--- src/WAV.jl
+++ src/WAV.jl
@@ -1,5 +1,14 @@
 
 using .WAV
 
-sink(file::String) = x -> sink(x,file)
-sink(x,file::String) = wavwrite(sink(x),file,Fs=samplerate(x))
+sink(x,to::String;length=nsamples(x)*frames,samplerate=samplerate(x)) = 
+    wavwrite(sink(x),file,Fs=samplerate(x))
+
+function signal(x::String,fs=missing)
+    x,_fs = wavread(x)
+    if !isconsistent(fs,_fs)
+        error("Expected file $x to have samplerate $fs. If you wish to convert",
+              " the sample rate, you can use `tosamplerate`.")
+    end
+    signal(x,_fs)
+end
--- src/files.jl
+++ src/files.jl
@@ -1,9 +1,2 @@
 
 # signals can be filenames
-function signal(x::String,fs=missing)
-    x,_fs = load(x)
-    if !isconsistent(fs,_fs)
-        error("Expected file $x to have samplerate $fs.")
-    end
-    signal(x,_fs)
-end
