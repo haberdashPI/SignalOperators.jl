ff5965bf5b501e4acb89e20519ec0e5d1f2336ff
haberdashpi <david.frank.little@gmail.com>
2019-09-17 18:05:31 +0000

    revised definition of `inflen`

--- src/signal.jl
+++ src/signal.jl
@@ -57,7 +57,12 @@ end
     inflen
 
 Represents an infinite length. Proper overloads are defined to handle 
-arithematic and ordering for the infinite value.
+arithematic and ordering for the infinite value. 
+
+## Missing values
+
+For the purposes of `SignalOperators` missing values are considered
+to be unknown, but of finite length. For example: `inflen * missing == inflen`.
 
 """
 const inflen = InfiniteLength()
@@ -70,19 +75,23 @@ Base.ismissing(::InfiniteLength) = false
 Base.:(+)(::InfiniteLength,::Number) = inflen
 Base.:(+)(::Number,::InfiniteLength) = inflen
 Base.:(-)(::InfiniteLength,::Number) = inflen
+Base.:(+)(::InfiniteLength,::Missing) = inflen
+Base.:(+)(::Missing,::InfiniteLength) = inflen
+Base.:(-)(::InfiniteLength,::Missing) = inflen
 Base.isless(::Number,::InfiniteLength) = true
 Base.isless(::InfiniteLength,::Number) = false
+Base.isless(::InfiniteLength,::Missing) = false
+Base.isless(::Missing,::InfiniteLength) = true
 Base.isless(::InfiniteLength,::InfiniteLength) = false
-Base.min(x::Number,::InfiniteLength) = x
-Base.min(::InfiniteLength,x::Number) = x
-Base.max(x::Number,::InfiniteLength) = inflen
-Base.max(::InfiniteLength,x::Number) = inflen
 Base.:(*)(::InfiniteLength,::Number) = inflen
 Base.:(*)(::Number,::InfiniteLength) = inflen
+Base.:(*)(::InfiniteLength,::Missing) = inflen
+Base.:(*)(::Missing,::InfiniteLength) = inflen
 Base.:(*)(::InfiniteLength,::Unitful.FreeUnits) = inflen
 Base.:(/)(::InfiniteLength,::Number) = inflen
 Base.:(/)(::InfiniteLength,::Missing) = inflen
 Base.:(/)(::Number,::InfiniteLength) = 0
+Base.:(/)(::Missing,::InfiniteLength) = 0
 
 """
 
